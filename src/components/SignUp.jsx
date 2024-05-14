6147ce9c3745e6ec13e0ae9987219dfd938ef5dd
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-14 10:09:12 +0530

    "building pages"

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import authService from "../appwrite/auth";
+import authService from "../appwrite/auth.js";
 import { Link, useNavigate } from "react-router-dom";
-import { login } from "../store/authSlice";
-import { Button, Input, Logo } from "./index";
+import { login } from "../store/authSlice.js";
+import { Button, Input, Logo } from "./index.js";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 
-function SignUp() {
+function Signup() {
   const navigate = useNavigate();
   const [error, setError] = useState("");
   const dispatch = useDispatch();
-  const [register, handleSubmit] = useForm();
+  const { register, handleSubmit } = useForm();
 
   const create = async (data) => {
     setError("");
@@ -51,43 +51,43 @@ function SignUp() {
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 
         <form onSubmit={handleSubmit(create)}>
-            <div className="space-y-5">
-                <Input
-                label="Full Name: "
-                placeholder="Enter your full name"
-                {...register("name"), {
-                    required: true
-                }}
-                />
-
-                <Input
-                label="Email: "
-                placeholder="Enter your email"
-                type="email"
-                {...register("email", {
-                    required: true,
-                    validate: {
-                        matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                        "Email address must be a valid address",
-                    }
-                })}
-                />
-
-                <Input
-                label="Password: "
-                type="password"
-                placeholder="Enter your password"
-                {...register("password", {
-                        required: true,})}
-                />
-                <Button type="submit" className="w-full">
-                    Create Account
-                </Button>
-            </div>
-        </form>l
+          <div className="space-y-5">
+            <Input
+              label="Full Name: "
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
+            <Input
+              label="Email: "
+              placeholder="Enter your email"
+              type="email"
+              {...register("email", {
+                required: true,
+                validate: {
+                  matchPatern: (value) =>
+                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                    "Email address must be a valid address",
+                },
+              })}
+            />
+            <Input
+              label="Password: "
+              type="password"
+              placeholder="Enter your password"
+              {...register("password", {
+                required: true,
+              })}
+            />
+            <Button type="submit" className="w-full">
+              Create Account
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
 }
 
-export default SignUp;
+export default Signup;
